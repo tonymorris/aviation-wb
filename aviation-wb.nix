5623bf4c262609dab9de6a091a8a91ee9debbb62
George Wilson <george@wils.online>
2018-06-19 11:27:46 +1000

    Fix the build
    Run this with nix-shell --argstr compiler ghc802

--- aviation-wb.nix
+++ aviation-wb.nix
@@ -1,12 +1,13 @@
 { mkDerivation, base, colour, diagrams-cairo, diagrams-core
-, diagrams-lib, lens, plots, stdenv
+, diagrams-lib, hgeometry, lens, mtl, plots, stdenv
 }:
 mkDerivation {
   pname = "aviation-wb";
   version = "0.0.1";
   src = ./.;
   libraryHaskellDepends = [
-    base colour diagrams-cairo diagrams-core diagrams-lib lens plots
+    base colour diagrams-cairo diagrams-core diagrams-lib hgeometry
+    lens mtl plots
   ];
   homepage = "https://github.com/tonymorris/aviation-wb";
   description = "Weight/Balance";
